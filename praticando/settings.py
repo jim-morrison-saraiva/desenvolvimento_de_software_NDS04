7dfafedb9020eaf47e48e2500086d29f456d7c31
barbara <b_nunesdemello@hotmail.com>
2025-08-26 21:27:17 -0400

    add a linha 145 em settings

--- praticando/settings.py
+++ praticando/settings.py
@@ -143,6 +143,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
